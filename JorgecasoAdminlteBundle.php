2d1e05473a56f33033f929bba9ac2d24bc47afbe
Jorge Caso <jl_cr3@hotmail.com>
2016-12-03 08:22:47 -0500

    Initial source

--- JorgecasoAdminlteBundle.php
+++ JorgecasoAdminlteBundle.php
@@ -3,17 +3,8 @@
 namespace Jorgecaso\AdminlteBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\Console\Application;
-use Sensio\Bundle\GeneratorBundle\Generator\DoctrineCrudGenerator;
-use Symfony\Component\Filesystem\Filesystem;
 
 class JorgecasoAdminlteBundle extends Bundle
 {
-    public function registerCommands(Application $application){
-        $crudCommand = $application->get('jabgenerate:doctrine:crud');
-        $generator = new DoctrineCrudGenerator(null, __DIR__.'/Resources/skeleton/crud');
-        $crudCommand->setGenerator($generator);
-
-        parent::registerCommands($application);
-    }
+    
 }
